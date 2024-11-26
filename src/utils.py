24713ebba4088b148a45d0d7e9b3cf718c6610bd
xlepotato <yucia181@gmail.com>
2024-11-26 11:26:38 +0800

    feat: add evaluate_models function in utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 
@@ -14,5 +17,33 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        # Iterate through all the models
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Perform prediction
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Evaluate the model on test set
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
     except Exception as e:
         raise CustomException(e, sys)
