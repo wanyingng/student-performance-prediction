6c0a69e3259741b26be2bde8cb6f6f69ea4ab61e
xlepotato <yucia181@gmail.com>
2024-11-26 12:46:49 +0800

    feat: add load_object function in utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
@@ -45,5 +46,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
