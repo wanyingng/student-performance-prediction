c7f3a709026599a485c79da2bf36d324c23a3fd5
xlepotato <yucia181@gmail.com>
2024-11-28 00:09:35 +0800

    refactor: format code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    if request.method =='GET':
+    if request.method == 'GET':
         return render_template('form.html')
     else:
         data = CustomData(
--- src/pipeline/predict.py
+++ src/pipeline/predict.py
@@ -10,19 +10,22 @@ class PredictPipeline:
     def __init__(self):
         pass
 
+
     def predict(self, features):
         try:
             model_path = os.path.join("artifacts", "model.pkl")
             preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+
             print("Start loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             print("Completed loading")
+
             X = preprocessor.transform(features)
             y_pred = model.predict(X)
             return y_pred
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
 
 class CustomData:
